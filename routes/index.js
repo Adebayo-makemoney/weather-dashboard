dcf78f2c02f3be0058e63e55654659d09e7015f3
Adebayo-makemoney <aoluwafisayomi2022@gmail.com>
2025-10-20 19:04:02 +0100

    weather

--- routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const postController = require('../controllers/postController');
-
-// Home page - show all posts
-router.get('/', postController.getAllPosts);
-
-module.exports = router;
